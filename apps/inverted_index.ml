468c870f46353313c530a44c24a0250174310b3b
HAOCHENG <hs454@cornell.edu>
2012-04-09 13:58:07 -0400

    added mkindex

--- apps/inverted_index.ml
+++ apps/inverted_index.ml
@@ -1,4 +1,25 @@
+#use "sequence.ml"
 (* inverted_index computes an inverted index for the contents of
  * a given file. The filename is the given string.
  * The results are output to stdout. *)
-let mkindex (args : string ) : unit = failwith "Implement me!"
+
+module Dict = Map.Make (
+    struct 
+        type t = string
+        let compare = compare
+    end
+)
+
+let mkindex (args : string ) : unit = 
+    let file = read_whole_file args in
+    let docs = load_documents file in
+    let s = Array.of_list docs in
+    
+    let map = map_reduce 
+    (fun x -> List.fold_left (fun a e -> if !(Dict.mem e a) then Dict.add e
+    [x.id] a else a) Dict.empty (split_words
+    x.contents)) Dict.emtpy acc s in 
+    Dict.
+    
+let acc = (fun a x -> List.fold_left( fun ac e -> if Dict.mem (fst e) ac then
+    Dict.add (fst e) (snd e)@(Dict.find (fst e) ac) ac else Dict.add (fst e)
